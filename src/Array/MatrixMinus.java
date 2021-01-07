181e1456ad102c81ca723ebca0d0b21de2099612
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 09:33:15 +0700

    Input number of rows for matrix

---
+++ src/Array/MatrixMinus.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MatrixMinus {
+    public static void main(String []args) {
+        static int m;
+        static int n;
+        static int[][] matrix;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number of rows for matrix: ");
+        n = sc.nextInt();
+
+    }
+
+}
