5c1d442ba5c7d2f0d02f15c9273e09a3cc66f4b9
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 10:51:14 +0700

    Input degree of matrix

---
+++ src/Array/MatrixDiagonal.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MatrixDiagonal {
+    public static void main(String []args) {
+        int sum = 0;
+        int degree;
+        int matrix[][];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input degree of matrix: ");
+        degree = sc.nextInt();
+
+    }
+}
