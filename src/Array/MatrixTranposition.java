68b2c7c3c00806bdec26e328ac1e614cc2403d2d
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 13:49:42 +0700

    show the matrix and transposition matrix

--- src/Array/MatrixTranposition.java
+++ src/Array/MatrixTranposition.java
@@ -10,7 +10,7 @@ public class MatrixTranposition {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input number of rows in matrix: ");
         n = sc.nextInt();
-        System.out.println("Input number of colums in matrix: ");
+        System.out.println("Input number of columns in matrix: ");
         m = sc.nextInt();
         matrix = new int[n][m];
         transMatrix = new int[m][n];
@@ -24,6 +24,33 @@ public class MatrixTranposition {
             System.out.println();
         }
         // Input elements of matrix
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transMatrix[i][j] = matrix[j][i];
+            }
+            System.out.println();
+        }
+        // Find elements in transposition matrix
+
+        System.out.println("Matrix is: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // Show the matrix
+
+        System.out.println("Transposition matrix: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(transMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
 
     }
 }
