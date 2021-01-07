ad2f453529e26a316563751f2ef78fe3c79a035f
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 11:25:53 +0700

    Input number of rows and colums in matrix

---
+++ src/Array/MatrixSum.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MatrixSum {
+    public static void main(String []args) {
+        int sum = 0;
+        int n,m;
+        int matrix[][];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number of rows in matrix: ");
+        n = sc.nextInt();
+        System.out.println("Input number of rows in matrix: ");
+        m = sc.nextInt();
+    }
+}
