a7419b5e8701781286680182e12b033f7a5d7b43
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 10:28:35 +0700

    Print the result

--- src/Array/MatrixMinus.java
+++ src/Array/MatrixMinus.java
@@ -4,46 +4,52 @@ import java.util.Scanner;
 
 public class MatrixMinus {
     public static void main(String []args) {
-        static int m;
-        static int n;
-        static int[][] matrixA;
-        static int[][] matrixB;
-        static int[][] matrixC;
+        int n,m;
+        int[][] matrixA;
+        int[][] matrixB;
+        int[][] matrixC;
         Scanner sc = new Scanner(System.in);
         System.out.println("Input number of rows for matrix: ");
         n = sc.nextInt();
-        System.out.println("Input number of column for matrix: ");
+        System.out.println("Input number of columns for matrix: ");
         m = sc.nextInt();
         matrixA = new int[n][m];
         matrixB = new int[n][m];
         matrixC = new int[n][m];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; i <= m; i++) {
-                System.out.print("Matrix A: ");
+        System.out.println("Matrix A: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("A["+i+","+j+"] ");
                 matrixA[i][j] = sc.nextInt();
             }
             System.out.println();
         }
         // input matrix A
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; i <= m; i++) {
-                System.out.print("Matrix B: ");
+        System.out.println("Matrix B: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("B["+i+","+j+"] ");
                 matrixB[i][j] = sc.nextInt();
             }
             System.out.println();
         }
         // input matrix B
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; i <= m; i++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 matrixC[i][j] = matrixA[i][j] - matrixB[i][j];
             }
             System.out.println();
         }
         // calculate elements of matrix C
 
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrixC[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
