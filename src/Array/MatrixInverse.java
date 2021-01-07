387ca8eccb2bd8298e84845e1843339db215f8e5
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 16:07:19 +0700

    Input degree of square matrix

---
+++ src/Array/MatrixInverse.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MatrixInverse {
+    public static void main (String []args) {
+        int n;
+        int matrix[n][n];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input degree of square matrix: ");
+        n = sc.nextInt();
+
+    }
+
+}
