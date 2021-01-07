2113485dc2ab3cd82adbdb2c3d11e471484613af
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 13:43:05 +0700

    Input elements of matrix

--- src/Array/MatrixTranposition.java
+++ src/Array/MatrixTranposition.java
@@ -14,5 +14,16 @@ public class MatrixTranposition {
         m = sc.nextInt();
         matrix = new int[n][m];
         transMatrix = new int[m][n];
+
+        System.out.println("Input matrix: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.println("A["+i+"]["+j+"]: ");
+                matrix[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+        // Input elements of matrix
+
     }
 }
