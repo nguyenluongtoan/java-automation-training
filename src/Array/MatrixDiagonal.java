6d15545d9a0f5e5666b6887f601e7980e5d9a4db
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 11:05:53 +0700

    calculate and print the sum

--- src/Array/MatrixDiagonal.java
+++ src/Array/MatrixDiagonal.java
@@ -25,10 +25,18 @@ public class MatrixDiagonal {
         System.out.println("Matrix is: ");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(A[i][j] + "\t");
+                System.out.print(matrix[i][j] + "\t");
             }
-            System.out.println("\n");
         }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("Sum of diagonal ia: " + sum);
     }
 }
