79faa183fcced7ca4dfc92cd62e31def247f83f6
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 10:38:40 +0700

    Initialize 2 matrixs with n rows and m columns

--- src/Array/MagicPlus.java
+++ src/Array/MagicPlus.java
@@ -9,5 +9,13 @@ public class MagicPlus {
         int[][] matrixB;
         int[][] matrixC;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Input number of rows for matrix: ");
+        n = sc.nextInt();
+        System.out.println("Input number of columns for matrix: ");
+        m = sc.nextInt();
+        matrixA = new int[n][m];
+        matrixB = new int[n][m];
+        matrixC = new int[n][m];
+
     }
 }
