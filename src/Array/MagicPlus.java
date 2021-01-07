17dfae8869312bf12ee7a594bf5817a565c7553e
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 10:36:33 +0700

    Initialize variables

---
+++ src/Array/MagicPlus.java
@@ -0,0 +1,13 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MagicPlus {
+    public static void main(String []args) {
+        int n,m;
+        int[][] matrixA;
+        int[][] matrixB;
+        int[][] matrixC;
+        Scanner sc = new Scanner(System.in);
+    }
+}
