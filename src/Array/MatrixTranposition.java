6daa96636094c86533144d399431194beec59664
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 13:38:14 +0700

    Input number of rows in matrix

---
+++ src/Array/MatrixTranposition.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MatrixTranposition {
+    public static void main (String []args) {
+        int n,m;
+        int matrix[][];
+        int transMatrix[][];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number of rows in matrix: ");
+
+    }
+}
