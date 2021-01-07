999a4e8a59ff7bfa2a4950744f0b29d9638055d5
sonhv1 <hongsn.1996@gmail.com>
2021-01-07 09:50:32 +0700

    calculate elements of matrix C

--- src/Array/MatrixMinus.java
+++ src/Array/MatrixMinus.java
@@ -6,30 +6,45 @@ public class MatrixMinus {
     public static void main(String []args) {
         static int m;
         static int n;
-        static int[][] matrix;
+        static int[][] matrixA;
+        static int[][] matrixB;
+        static int[][] matrixC;
         Scanner sc = new Scanner(System.in);
         System.out.println("Input number of rows for matrix: ");
         n = sc.nextInt();
         System.out.println("Input number of column for matrix: ");
         m = sc.nextInt();
-        matrix = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; i < m; i++) {
+        matrixA = new int[n][m];
+        matrixB = new int[n][m];
+        matrixC = new int[n][m];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; i <= m; i++) {
                 System.out.print("Matrix A: ");
-                matrix[i][j] = sc.nextInt();
+                matrixA[i][j] = sc.nextInt();
             }
             System.out.println();
         }
         // input matrix A
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; i < m; i++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; i <= m; i++) {
                 System.out.print("Matrix B: ");
-                matrix[i][j] = sc.nextInt();
+                matrixB[i][j] = sc.nextInt();
             }
             System.out.println();
         }
         // input matrix B
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; i <= m; i++) {
+                matrixC[i][j] = matrixA[i][j] - matrixB[i][j];
+            }
+            System.out.println();
+        }
+        // calculate elements of matrix C
+
+
+
     }
 
 }
