48799ab56f24ae4e226bb22c3b2f4063908b4e36
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-08 10:07:22 +0700

    Identify elements of new matrix

--- src/Array/MatrixMulti.java
+++ src/Array/MatrixMulti.java
@@ -1,2 +1,23 @@
-package Array;public class MatrixMulti {
+package Array;
+
+public class MatrixMulti {
+    public static void maiṇ̣̣̣̣̣̣̣̣(String[] args) {
+
+    }
+
+    int[][] matrixMulti(int[][] a, int[][] b) {
+        int[][] c = new int[a.length][b[0].length];
+        int m = a.length;
+        int n = b[0].length;
+        int p = a[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < p; k++) {
+                    c[i][j] += a[i][p] * b[j][p];
+                }
+            }
+        }
+        return c;
+    }
 }
+
