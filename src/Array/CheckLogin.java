2df4d461aef9202b7e3d1851e48f0d5495390ccd
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-08 14:27:51 +0700

    create checkLogin function

---
+++ src/Array/CheckLogin.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class CheckLogin {
+    public static void main (String []args) {
+        String []username = new String[]{"sonhv1", "sonhv2", "sonhv3"};
+        String []password = new String[]{"123", "456", "789"};
+
+
+    }
+
+    public static boolean checkLogin(boolean check) {
+
+        return check;
+    }
+}
