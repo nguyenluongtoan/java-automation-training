565c8dd00f44569453f36bcb8395f17d437ff795
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-11 13:30:05 +0700

    input 2 strings

---
+++ src/String/Prob2.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.Scanner;
+
+public class Prob2 {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input first string: ");
+        String s1 = scanner.nextLine();
+        System.out.println("Input second string: ");
+        String s2 = scanner.nextLine();
+
+    }
+}
