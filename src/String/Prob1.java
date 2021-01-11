76226dc0171270eabe574ff8aa2f47efd5f63756
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-11 13:27:18 +0700

    input 2 strings and print the result of concat

---
+++ src/String/Prob1.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Prob1 {
+    public static void main (String []args) {
+        String s1 = "Hello";
+        String s2 = "world";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+    }
+
+}
