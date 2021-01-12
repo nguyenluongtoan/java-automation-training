7222494cef42e82c1b2142a889092d321b1da07e
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-12 13:46:36 +0700

    create an arraylist and add elements to list

---
+++ src/ArrayList/Prob2.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Prob2 {
+    public static void main(String []args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+    }
+}
