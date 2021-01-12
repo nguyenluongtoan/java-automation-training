f49491b6f8eaf8e4b45a787c3379cccd26a4db21
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-12 10:30:44 +0700

    Input string from keyboard then call removeDuplicate function with parameter is that string

--- src/String/Prob5.java
+++ src/String/Prob5.java
@@ -1,11 +1,17 @@
 package String;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Prob5 {
     public static void main(String []args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the string: ");
+        String str = scanner.nextLine();
+        scanner.close();
+        System.out.println("The string after remove duplicated characters is: " + removeDuplicate(str));
+        // removeDuplicate(str);
     }
 
     public static String removeDuplicate (String str) {
