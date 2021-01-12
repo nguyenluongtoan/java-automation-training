d59e1d90e4769874128163166e861fdfc4e2dd89
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-12 10:27:18 +0700

    append each character to new string when it's not in hashset

--- src/String/Prob5.java
+++ src/String/Prob5.java
@@ -8,9 +8,16 @@ public class Prob5 {
 
     }
 
-    public static  String removeDuplicate (String str) {
-        Set(Character) hs = new HashSet<>();
+    public static String removeDuplicate (String str) {
+        Set<Character> hs = new HashSet<>();
         StringBuilder newString = new StringBuilder();
-
+        for (int i = 0; i < str.length(); i++) {
+            if(!hs.contains(str.charAt(i))) {
+                newString.append(str.charAt(i));
+                hs.add(str.charAt(i));
+            }
+        }
+        return newString.toString();
     }
+
 }
