f330aa4b3a812b8a973ad86f643d0d938c9c65cd
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-12 13:59:33 +0700

    create an array list

---
+++ src/ArrayList/Prob3.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prob3 {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println("Input number of elements in array list: ");
+        int size = scanner.nextInt();
+
+    }
+}
