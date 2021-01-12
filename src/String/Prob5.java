a641905b39f935372d21dab144412fa96c097ffc
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-12 10:17:44 +0700

    Create a hashset and string builder to store new string after remove duplicated characters

---
+++ src/String/Prob5.java
@@ -0,0 +1,16 @@
+package String;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Prob5 {
+    public static void main(String []args) {
+
+    }
+
+    public static  String removeDuplicate (String str) {
+        Set(Character) hs = new HashSet<>();
+        StringBuilder newString = new StringBuilder();
+
+    }
+}
