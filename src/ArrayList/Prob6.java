043be7be909bcac2a27bee5a0d26d738a21c1766
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-13 09:52:49 +0700

    find max value of list

---
+++ src/ArrayList/Prob6.java
@@ -0,0 +1,26 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prob6 {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println("Input number of elements in array: ");
+        int size = scanner.nextInt();
+        System.out.println("Input elements of array: ");
+        for (int i = 0; i < size; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        int max = list.get(0);
+        int max2 = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+
+    }
+}
