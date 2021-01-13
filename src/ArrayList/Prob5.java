7ca3d2a1d7cdddd70e84cbf306274cfed42ddf33
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-13 09:24:20 +0700

    Input number of elements in array

---
+++ src/ArrayList/Prob5.java
@@ -0,0 +1,13 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prob5 {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println("Input number of elements in array: ");
+        int size = scanner.nextInt();
+    }
+}
