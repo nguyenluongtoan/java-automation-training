87a623da74fbc5128ac4385547a0663737299e7c
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-13 09:11:25 +0700

    Input number of elements in array

---
+++ src/ArrayList/Prob4.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prob4 {
+    public static void main(String []args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println("Input number of elements in array: ");
+        int size = scanner.nextInt();
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+
+        }
+
+    }
+}
