91f9d2b9017a6d09fb37fc82ea30ce050d054e29
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-14 14:37:03 +0700

    constructor

---
+++ src/ClassAndObject/Account.java
@@ -0,0 +1,17 @@
+package ClassAndObject;
+
+public class Account {
+    private int id;
+    private String name;
+    private int balance;
+
+    public Account(){}
+
+    public Account(int id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    
+}
