2d8cee81e83671be6578fe8a5cc3412f03fdbfdf
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-14 13:29:18 +0700

    constructor

--- src/ClassAndObject/Employee.java
+++ src/ClassAndObject/Employee.java
@@ -4,6 +4,14 @@ public class Employee {
     private int id;
     private String firstName;
     private String lastName;
-    private String salary;
+    private int salary;
 
+    public Employee() {}
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
 }
