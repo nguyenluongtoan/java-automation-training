b2b31cabb3ba3758f9d9d5caf19b4c5049caf3eb
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-14 10:42:45 +0700

    default constructor and constructor with parameter radius

---
+++ src/ClassAndObject/Prob1.java
@@ -0,0 +1,14 @@
+package ClassAndObject;
+
+public class Prob1 {
+    private double radius;
+    private String color;
+    public Prob1 () {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Prob1(double radius) {
+        this.radius = radius;
+    }
+}
