3e8ebab6b6416637cc950f3feb07bdac59adc004
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-14 11:20:06 +0700

    default constructor and constructor with 2 params length and width

---
+++ src/ClassAndObject/Rectangle.java
@@ -0,0 +1,14 @@
+package ClassAndObject;
+
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+}
