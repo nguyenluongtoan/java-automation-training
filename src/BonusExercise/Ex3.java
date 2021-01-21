7750fff145bd96b5eb5b95cdece31c647247afae
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-21 13:54:47 +0700

    sort elements in array by ascending order

--- src/BonusExercise/Ex3.java
+++ src/BonusExercise/Ex3.java
@@ -13,7 +13,16 @@ public class Ex3 {
         for (int i = 0; i < num; i++) {
             arr[i] = scanner.nextInt();
         }
-
+        int temp;
+        for (int i = 0; i < num -1; i++) {
+            for (int j = 0; j < num -1 - i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
 
     }
 }
