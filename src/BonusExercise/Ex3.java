db35871093c267e63a19952005009c5d9f43edb6
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-21 13:50:23 +0700

    input number of elements in array

---
+++ src/BonusExercise/Ex3.java
@@ -0,0 +1,12 @@
+package BonusExercise;
+
+import java.util.Scanner;
+
+// Description: Sort elements of array by ascending and descending order
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of elements in array: ");
+        int num = scanner.nextInt();
+    }
+}
