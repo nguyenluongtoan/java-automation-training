e2306fbecb1a7307916a9b00bbc21c6ff71b4da5
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-21 14:08:04 +0700

    function sort by asc and des order

--- src/BonusExercise/Ex3.java
+++ src/BonusExercise/Ex3.java
@@ -13,16 +13,46 @@ public class Ex3 {
         for (int i = 0; i < num; i++) {
             arr[i] = scanner.nextInt();
         }
+        Ex3 e = new Ex3();
+        System.out.println("The descending order: ");
+        e.sortByDes(arr);
+        System.out.println("The ascending order: ");
+        e.sortByAsc(arr);
+    }
+        public void sortByAsc(int a[]){
+            int temp;
+            for (int i = 0; i < a.length - 1; i++) {
+                for (int j = 0; j < a.length - 1 - i; j++) {
+                    if (a[j] > a[j + 1]) {
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i] + "\t");
+            }
+
+        }
+        // Sort by asc order
+
+    public void sortByDes(int b[]){
         int temp;
-        for (int i = 0; i < num -1; i++) {
-            for (int j = 0; j < num -1 - i; j++) {
-                if(arr[j] > arr[j+1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+        for (int i = 0; i < b.length - 1; i++) {
+            for (int j = 0; j < b.length - 1 - i; j++) {
+                if (b[j] < b[j + 1]) {
+                    temp = b[j];
+                    b[j] = b[j + 1];
+                    b[j + 1] = temp;
                 }
             }
         }
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i] + "\t");
+        }
+    }
+
 
     }
-}
+
