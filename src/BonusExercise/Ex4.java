c71a72fe186792440ae4f168e2c2f831b2758af0
sonhv1 <76928751+sonhv1@users.noreply.github.com>
2021-01-21 14:16:23 +0700

    create a random object and print random numbers in the range from 0 to 100

--- src/BonusExercise/Ex4.java
+++ src/BonusExercise/Ex4.java
@@ -1,5 +1,6 @@
 package BonusExercise;
 
+import java.util.Random;
 import java.util.Scanner;
 
 // Description: Nhập vào số lượng các ố random và on ra các số random
@@ -9,6 +10,11 @@ public class Ex4 {
         System.out.println("Input number of random numbers: ");
         int n = scanner.nextInt();
 
+        Random rd = new Random();
+        System.out.println("Random numbers are: ");
+        for (int i = 1; i <= n ; i++) {
+            System.out.println(rd.nextInt(100));
+        }
     }
 
 
